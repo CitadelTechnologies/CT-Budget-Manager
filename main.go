7ce95c336732b4969aa8365bfc9cea259884b263
Axel Venet <kern046@gmail.com>
2015-06-23 17:06:48 +0200

    Add basic messages from the server for shell visualization

--- main.go
+++ main.go
@@ -4,14 +4,22 @@ import(
 	"budget-manager/manager"
 	"log"
 	"net/http"
+	"fmt"
 )
 
 func main() {
 
+	fmt.Println("MongoDB server initialization")
+
 	manager.InitMongo()
 	defer manager.MongoDBConnection.Close()
 
+	fmt.Println("MongoDB is ready")
+	fmt.Println("Router initialization")
+
 	router := NewRouter()
+
+	fmt.Println("Server is ready")
 
 	log.Fatal(http.ListenAndServe(":28515", router))
 }
