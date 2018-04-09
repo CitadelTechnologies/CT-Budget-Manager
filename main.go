57df4c8c43a5753bbc304415790899ab4dbc7a3e
Axel Venet <kern046@gmail.com>
2018-04-09 13:29:29 +0200

    Fix the server port to 80

--- main.go
+++ main.go
@@ -3,9 +3,8 @@ package main
 import(
     "log"
     "net/http"
-    "os"
 )
 
 func main() {
-    log.Fatal(http.ListenAndServe(":" + os.Getenv("SERVER_PORT"), NewRouter()))
+    log.Fatal(http.ListenAndServe(":80", NewRouter()))
 }
