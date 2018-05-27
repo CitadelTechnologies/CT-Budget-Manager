226dfa214d1f5b82b50d08759333284e18267d1e
Axel Venet <kern046@gmail.com>
2018-05-27 13:31:02 +0200

    Secure process_at field in transaction creation method

--- transaction/controller.go
+++ transaction/controller.go
@@ -1,6 +1,7 @@
 package transaction
 
 import(
+	"ct-budget-manager/exception"
 	"ct-budget-manager/server"
 	"net/http"
 	"time"
@@ -12,10 +13,12 @@ func CreateTransactionAction(w http.ResponseWriter, r *http.Request) {
 
 	params := mux.Vars(r)
 	data := server.DecodeJsonRequest(r)
-	var isset bool
 	processedAt := time.Now()
-	if processedAt, isset = data["processed_at"]; isset {
-		processedAt = time.Parse(time.RFC3339, data.(string))
+	if date, isset := data["processed_at"]; isset {
+		var err error
+		if processedAt, err = time.Parse(time.RFC3339, date.(string)); err != nil {
+			panic(exception.New(400, "Invalid process_at value", err))
+		}
 	}
 	transaction := CreateTransaction(
 		params["budget"],
